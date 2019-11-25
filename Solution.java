aaa88701c889c0d7e6e1fb6b84bbc252cf8a0c27
TangsongyuanmingQingHad <49867878+TangsongyuanmingQingHad@users.noreply.github.com>
2019-11-25 12:51:45 +0800

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -2,14 +2,7 @@ package com.had.Leetcode.TwoSum01;
 
 import java.util.HashMap;
 
-/**
- * 将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
- * <p>
- * 示例：
- * <p>
- * 输入：1->2->4, 1->3->4
- * 输出：1->1->2->3->4->4
- */
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
        if(nums == null || nums.length < 2) {
