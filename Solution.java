3f38d5da5b296c1336df694efeff121b5882345c
TangsongyuanmingQingHad <49867878+TangsongyuanmingQingHad@users.noreply.github.com>
2019-12-02 22:35:00 +0800

    小组第一天
    isAnagram

---
+++ Solution.java
@@ -0,0 +1,43 @@
+package com.pqst.had.LeetCode;
+
+import java.util.Arrays;
+
+/**
+ * Given two strings s and t , write a function to determine if t is an anagram of s.
+ *
+ * Example 1:
+ *
+ * Input: s = "anagram", t = "nagaram"
+ * Output: true
+ * Example 2:
+ *
+ * Input: s = "rat", t = "car"
+ * Output: false
+ * Note:
+ * You may assume the string contains only lowercase alphabets.
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/valid-anagram
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution {
+    public static void main(String[] args) {
+        String s = "cat";
+        String t = "act";
+
+        boolean flag = isAnagram(s, t);
+        System.out.println(flag);
+    }
+
+    public static boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()) {
+            return false;
+        }
+
+        char[] str1 = s.toCharArray();
+        char[] str2 = t.toCharArray();
+        Arrays.sort(str1);
+        Arrays.sort(str2);
+        return String.valueOf(str1).equals(String.valueOf(str2));
+    }
+}
